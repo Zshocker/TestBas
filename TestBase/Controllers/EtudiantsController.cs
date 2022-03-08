d781753942833c610e994665d918548ed46a9b38
Zshocker <hicham.sentel-etu@etu.univh2c.ma>
2022-03-08 18:06:49 +0100

    xx

--- TestBase/Controllers/EtudiantsController.cs
+++ TestBase/Controllers/EtudiantsController.cs
@@ -28,27 +28,36 @@ namespace TestBase.Controllers
         [HttpGet]
         public List<Etudiant> GetAllEtudiants()
         {
-            _logger.LogWarning("["+DateTime.Now.ToString()+"] : All Etudiants where requested by "+ Request.HttpContext.Connection.RemoteIpAddress);
+
+            _logger.LogWarning("[" + DateTime.Now.ToString() + "] : All Etudiants where requested by " + Request.HttpContext.Connection.RemoteIpAddress);
             return _context.etudiants.ToList();
         }
-        [HttpPost]
-        public Etudiant DetailEtudiant(int? id)
+        [HttpGet("{id}")]
+        public Etudiant DetailEtudiant(int id)
         {
-            if (id == null)
-            {
-                _logger.LogCritical("[" + DateTime.Now.ToString() + "] : Fatal Error : request by " + Request.HttpContext.Connection.RemoteIpAddress + " doesn't include id ");
-                return null;
-            }
-            var etudiant =  _context.etudiants.FirstOrDefault(m => m.id == id);
+            var etudiant = _context.etudiants.FirstOrDefault(m => m.id == id);
             if (etudiant == null)
             {
-                _logger.LogError("[" + DateTime.Now.ToString() + "] : Error : id requested by " + Request.HttpContext.Connection.RemoteIpAddress+ " was not found ");
+                _logger.LogError("[" + DateTime.Now.ToString() + "] : Error : id requested by " + Request.HttpContext.Connection.RemoteIpAddress + " was not found ");
 
                 return null;
             }
-            _logger.LogWarning("[" + DateTime.Now.ToString() + "] :Etudiant "+etudiant.ToString()+" was requested by " + Request.HttpContext.Connection.RemoteIpAddress);
+            _logger.LogWarning("[" + DateTime.Now.ToString() + "] :Etudiant " + etudiant.ToString() + " was requested by " + Request.HttpContext.Connection.RemoteIpAddress);
             return etudiant;
         }
-        
+        [HttpPost]
+        public bool InsertEtudiant(Etudiant etudiant)
+        {
+            try
+            {
+                _context.Add(etudiant);
+                _context.SaveChanges();
+                return true;
+            }catch(Exception e)
+            {
+                return false;
+            }
+
+        }
     }
 }
